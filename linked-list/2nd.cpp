162037c0fc9a2f5efbfc1431e37403d429347852
Arisha598 <arishachaudhary56@gmail.com>
2024-03-03 11:34:31 +0530

    linked-list

---
+++ linked-list/2nd.cpp
@@ -0,0 +1,71 @@
+//insertion at Head
+
+#include <iostream>
+using namespace std;
+class Node{
+  public:
+  int data;
+  Node* prev;
+  Node* next;
+  Node(){
+    this->data=0;
+    this->prev=NULL;
+    this->next=NULL;
+  }
+  Node(int data){
+    this->data=data;
+    this->prev=NULL;
+    this->next=NULL;
+
+  }
+};
+void print(Node* head){
+  Node* temp =head;
+  while(temp!=NULL){
+  cout<<temp->data<<" ";
+  temp=temp->next;
+  }
+}
+int getLength(Node* head){
+  int len =0;
+  Node* temp=head;
+  while(temp!=NULL){
+    temp=temp->next;
+    len++;
+  }
+  return len;
+}
+void insertatHead(Node* &head,Node*  &tail,int data){
+  if(head == NULL){
+    Node* newNode = new Node(data);
+    head=newNode;
+    tail=newNode;
+  }
+  else{
+  Node*newNode = new Node(data);
+  newNode->next=head;
+  head->prev=newNode;
+  head=newNode;
+  }
+};
+int main() {
+Node*first=new Node(10);
+Node*second=new Node(20);
+Node*third=new Node(30);
+
+Node* head=first;
+Node*tail=third;  
+
+
+first->next=second;
+second->prev=first;
+
+second->next=third;
+third->prev=second;
+
+print(first);
+cout<<endl;
+
+insertatHead(head,tail,140);
+print(head);
+}
